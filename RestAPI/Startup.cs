3e492abe5b5a97b160d30e9bd4192e1036e5b9f8
DESKTOP-OMKQAFS\User <lilit.qocharyan35835@gmail.com>
2022-11-08 13:57:23 +0400

    AddNewtonsoftJson

--- RestAPI/Startup.cs
+++ RestAPI/Startup.cs
@@ -46,6 +46,10 @@ namespace RestAPI
             {
                 options.UseSqlServer(Configuration.GetConnectionString("StoreEntities"));
             });
+
+            services.AddControllers().AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
